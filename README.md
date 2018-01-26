7023cef0f5398239098390109f9c99d0c4db4d9a
Richard Juevesano <richard@sym.ph>
2018-01-26 13:09:54 +0800

    remove appengine

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # richardandchristy
-
-Wedding website...
---
+++ assets/js/animations.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+   //Only animate elements when using non-mobile devices
+    if (isMobile.any === false) {
+
+        /* Animate elements in #hero */
+        $('#hero .statement').css('opacity', 0).one('inview', function(isInView) {
+            if (isInView) {$(this).addClass('animated bounceIn delayp4');}
+        });
+
+        /* Animate elements in #wedding */
+        $('#wedding .title-text').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated fadeInUp delayp2');}
+        });
+
+        /* Animate elements in #story */
+        $('#story .title-text').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated fadeInUp delayp2');}
+        });
+
+        /* Animate elements in #gallery */
+        $('#gallery .title-text').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated fadeInUp delayp2');}
+        });
+
+        /* Animate elements in #video */
+        $('#video .title-text').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated fadeInUp delayp2');}
+        });
+
+        /* Animate elements in #gift */
+        $('#gift .title-text').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated fadeInUp delayp2');}
+        });
+
+        /* Animate elements in #gift */
+        $('#gift .couple-profile').css('opacity', 0).one('inview', function(event, isInView) {
+            if (isInView) {$(this).addClass('animated bounceIn delayp3');}
+        });
+
+    }
+
+});
